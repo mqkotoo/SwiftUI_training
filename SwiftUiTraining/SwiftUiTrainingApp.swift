9afb8ee4dcd326e8c679924960e44264d69d826e
mqkotoo <ilikekiss@icloud.com>
2026-06-03 12:20:44 +0900

    Initial Commit

---
+++ SwiftUiTraining/SwiftUiTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTrainingApp.swift
+//  SwiftUiTraining
+//
+//  Created by makoto.a.murakami on 2026/06/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUiTrainingTests/SwiftUiTrainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTrainingTests.swift
+//  SwiftUiTrainingTests
+//
+//  Created by makoto.a.murakami on 2026/06/03.
+//
+
+import Testing
+@testable import SwiftUiTraining
+
+struct SwiftUiTrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
