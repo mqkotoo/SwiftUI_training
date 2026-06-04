ee18fa318e3565db133dbaa247ebf39a9f2d531e
mqkotoo <ilikekiss@icloud.com>
2026-06-04 14:17:53 +0900

    レイアウト作成

--- SwiftUiTraining/ContentView.swift
+++ SwiftUiTraining/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "globe")
+                    .foregroundColor(Color.red)
+                    .imageScale(.large)
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color.green)
+            }
+            Text("Good evening, world!")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundColor(Color.red)
         }
-        .padding()
     }
 }
 
