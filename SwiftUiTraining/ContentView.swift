f8007977c6dea52e8177e33c0522245c90786f82
mqkotoo <ilikekiss@icloud.com>
2026-06-08 12:33:20 +0900

    Listにする

--- SwiftUiTraining/ContentView.swift
+++ SwiftUiTraining/ContentView.swift
@@ -9,17 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Image(.gitHubMark)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 44.0, height: 44.0)
-            VStack(alignment: .leading) {
-                Text("Owner Name")
-                    .font(.caption)
-                Text("Repository Name")
-                    .font(.body)
-                    .fontWeight(.semibold)
+        
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            HStack{
+                Image(.gitHubMark)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 44.0, height: 44.0)
+                VStack(alignment: .leading) {
+                    Text("Owner Name")
+                        .font(.caption)
+                    Text("Repository Name")
+                        .font(.body)
+                        .fontWeight(.semibold)
+                }
             }
         }
     }
