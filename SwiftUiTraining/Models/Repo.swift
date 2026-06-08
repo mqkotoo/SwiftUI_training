fc6b19a4bbc2d95123a05c762b543f3b7ec9d4ee
mqkotoo <ilikekiss@icloud.com>
2026-06-08 12:48:27 +0900

    model作成

---
+++ SwiftUiTraining/Models/Repo.swift
@@ -0,0 +1,14 @@
+//
+//  Repo.swift
+//  SwiftUiTraining
+//
+//  Created by makoto.a.murakami on 2026/06/08.
+//
+
+import Foundation
+
+struct Repo: Identifiable {
+    var id: Int
+    var name: String
+    var owner: User
+}
