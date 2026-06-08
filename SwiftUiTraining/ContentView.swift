330e97531bd6b704d6f75b02567744b39ab0fbea
mqkotoo <ilikekiss@icloud.com>
2026-06-08 17:23:41 +0900

    リストの中身を渡して表示

--- SwiftUiTraining/ContentView.swift
+++ SwiftUiTraining/ContentView.swift
@@ -8,18 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let mockRepos = [
+        Repo(
+            id: 1,
+            name: "Test Repo1",
+            owner: User(name: "Test User1")
+        ),
+        Repo(
+            id: 2,
+            name: "Test Repo2",
+            owner: User(name: "Test User2")
+        ),
+        Repo(
+            id: 3,
+            name: "Test Repo3",
+            owner: User(name: "Test User3")
+        ),
+        Repo(
+            id: 4,
+            name: "Test Repo4",
+            owner: User(name: "Test User4")
+        ),
+        Repo(
+            id: 5,
+            name: "Test Repo5",
+            owner: User(name: "Test User5")
+        )
+    ]
     var body: some View {
-        
-        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-            HStack{
+        List(mockRepos) { repo in
+            HStack {
                 Image(.gitHubMark)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 44.0, height: 44.0)
                 VStack(alignment: .leading) {
-                    Text("Owner Name")
+                    Text(repo.owner.name)
                         .font(.caption)
-                    Text("Repository Name")
+                    Text(repo.name)
                         .font(.body)
                         .fontWeight(.semibold)
                 }
