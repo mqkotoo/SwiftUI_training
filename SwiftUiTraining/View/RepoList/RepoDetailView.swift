4fa6707eb42b798918b0d2238162e3606a1041d4
mqkotoo <ilikekiss@icloud.com>
2026-06-10 16:58:14 +0900

    view作成

---
+++ SwiftUiTraining/View/RepoList/RepoDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  RepoDetailView.swift
+//  SwiftUiTraining
+//
+//  Created by makoto.a.murakami on 2026/06/10.
+//
+
+import Foundation
+import SwiftUI
+
+struct RepoDetailView: View {
+    let repo: Repo
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(.gitHubMark)
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                    Text(repo.owner.name)
+                        .font(.caption)
+                }
+                Text(repo.name)
+                    .font(.body)
+                    .fontWeight(.bold)
+                // Optional Binding を使って、descriptionがnilじゃない場合のみUIを表示する
+                if let description = repo.description {
+                    Text(description)
+                        .padding(.top, 4) // .top などの方向と余白の長さを指定することができます
+                }
+                HStack {
+                    Image(systemName: "star")
+                    Text("\(repo.stargazersCount) stars")
+                }
+                .padding(.top, 8)
+                Spacer()
+            }
+            Spacer()
+        }
+        .padding(8)
+    }
+}
+
+#Preview {
+    RepoDetailView(repo: .mock1)
+}
