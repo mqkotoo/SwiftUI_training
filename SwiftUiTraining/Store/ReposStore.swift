219b6d3e22c9871c85d35d1f93d18162a3314047
mqkotoo <ilikekiss@icloud.com>
2026-06-16 18:30:43 +0900

    Apiからデータを取得

--- SwiftUiTraining/Store/ReposStore.swift
+++ SwiftUiTraining/Store/ReposStore.swift
@@ -11,9 +11,30 @@ import Foundation
 @MainActor
 final class ReposStore {
     private(set) var repos = [Repo]()
+    // decoderに変換設定付与
+    var decoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
 
     func loadRepos() async {
-        try? await Task.sleep(for: .seconds(2))
-        repos = [.mock1, .mock2, .mock3, .mock4, .mock5]
+        let url = URL(string: "https://api.github.com/orgs/mixigroup/repos")!
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.allHTTPHeaderFields = [
+            "Accept": "application/vnd.github+json"
+        ]
+        // GitHub API のリクエスト数制限(60回/h)回避のためのキャッシュ設定 ※研修内容とは直接関係ありません
+        urlRequest.cachePolicy = .returnCacheDataElseLoad
+        do {
+            // データ取得
+            let (data, _) = try await URLSession.shared.data(for: urlRequest)
+            // デコード処理
+            repos = try decoder.decode([Repo].self, from: data)
+        } catch {
+            // TODO: エラーハンドリングする
+            print(error)
+        }
     }
 }
