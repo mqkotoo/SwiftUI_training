a633ae947c141ab5fe1ceecb7228450f3b18eab6
mqkotoo <ilikekiss@icloud.com>
2026-06-18 18:26:03 +0900

    表示画面の出しわけ、NavigationStackの文字をGroupを使って全部のviewに適用

--- SwiftUiTraining/View/RepoList/RepoListView.swift
+++ SwiftUiTraining/View/RepoList/RepoListView.swift
@@ -9,21 +9,39 @@ import SwiftUI
 
 struct RepoListView: View {
     @Environment(ReposStore.self) private var store
-
     var body: some View {
         NavigationStack {
-            if store.repos.isEmpty {
-                ProgressView("loading...")
-            } else {
-                List(store.repos) { repo in
-                    NavigationLink(value: repo) {
-                        RepoRow(repo: repo)
+            Group{
+                if store.error != nil {
+                    VStack {
+                        Text("Failed to load repositories")
+                        Button(
+                            action: {
+                                Task {
+                                    // リトライボタンをタップしたときに再度リクエストを投げる
+                                    await store.loadRepos()
+                                }
+                            },
+                            label: {
+                                Text("Retry")
+                            }
+                        )
+                        .padding()
+                    }
+                } else
+                if store.isLoading {
+                    ProgressView("loading...")
+                } else {
+                    List(store.repos) { repo in
+                        NavigationLink(value: repo) {
+                            RepoRow(repo: repo)
+                        }
                     }
                 }
-                .navigationTitle("Repositories")
-                .navigationDestination(for: Repo.self) { repo in
-                    RepoDetailView(repo: repo)
-                }
+            }
+            .navigationTitle("Repositories")
+            .navigationDestination(for: Repo.self) { repo in
+                RepoDetailView(repo: repo)
             }
         }
         .task {
