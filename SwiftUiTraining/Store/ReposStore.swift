5e418923859ba5c0158806b4bed68436c98def6d
mqkotoo <ilikekiss@icloud.com>
2026-06-18 17:15:13 +0900

    error,isLoadingを作成、guardでエラーを投げる

--- SwiftUiTraining/Store/ReposStore.swift
+++ SwiftUiTraining/Store/ReposStore.swift
@@ -11,13 +11,15 @@ import Foundation
 @MainActor
 final class ReposStore {
     private(set) var repos = [Repo]()
+    private(set) var error: Error? = nil
+    private(set) var isLoading: Bool = false
     // decoderに変換設定付与
     private let decoder: JSONDecoder = {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         return decoder
     }()
-
+    
     func loadRepos() async {
         let url = URL(string: "https://api.github.com/orgs/mixigroup/repos")!
         var urlRequest = URLRequest(url: url)
@@ -29,12 +31,17 @@ final class ReposStore {
         urlRequest.cachePolicy = .returnCacheDataElseLoad
         do {
             // データ取得
-            let (data, _) = try await URLSession.shared.data(for: urlRequest)
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                throw URLError(.badServerResponse)
+            }
             // デコード処理
             repos = try decoder.decode([Repo].self, from: data)
+            isLoading = false
         } catch {
-            // TODO: エラーハンドリングする
             print(error)
+            self.error = error
+            isLoading = false
         }
     }
 }
